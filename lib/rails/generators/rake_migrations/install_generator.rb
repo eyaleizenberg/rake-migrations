2378e5b983e79f789cc6bbd77f41b4e246d729ce
Eyal Eizenberg <eyal.eizenberg+github@samanage.com>
2014-08-06 11:17:18 +0300

    create the post merge hook to call the script

--- lib/rails/generators/rake_migrations/install_generator.rb
+++ lib/rails/generators/rake_migrations/install_generator.rb
@@ -11,6 +11,20 @@ module RakeMigrations
     def create_migration_file
       migration_template "migration.rb", "db/migrate/create_rake_migrations_table.rb"
       template("rake_migrations_check.rb", "config/rake_migrations_check.rb")
+      write_to_post_merge_hook
+    end
+
+    def write_to_post_merge_hook
+      post_merge_file = ".git/hooks/post-merge"
+      what_to_write = <<-TEXT
+#{!File.exists?(post_merge_file) ? '#!/bin/sh' : ''}
+ruby ./config/rake_migrations_check.rb
+TEXT
+
+      File.open(post_merge_file, 'a+') do |file|
+        file.write(what_to_write) unless file.read.match(/rake_migrations_check/)
+      end
+      `chmod 777 #{post_merge_file}`
     end
   end
 end
--- lib/rails/generators/rake_migrations/templates/rake_migrations_check.rb
+++ lib/rails/generators/rake_migrations/templates/rake_migrations_check.rb
@@ -1,22 +1,28 @@
 require 'mysql2'
 
-client = Mysql2::Client.new(host: "localhost", username: "root", database: "samanage_development")
-results = client.query("select * from rake_migrations").map {|res| res["version"] }
-rake_migrations_lib = "#{`pwd`.strip}/lib/tasks/rake_migrations/*"
+module RakeMigrationsCheck
+  def self.check
+    client = Mysql2::Client.new(host: "localhost", username: "root", database: "samanage_development")
+    results = client.query("select * from rake_migrations").map {|res| res["version"] }
+    rake_migrations_lib = "#{`pwd`.strip}/lib/tasks/rake_migrations/*"
 
-rake_files = Dir[rake_migrations_lib].map do |file|
-  if !results.include?(file[/\d+/])
-    file = File.read(file)
-    namespace = file[/namespace :(.*?)do/m, 1].strip
-    task = file[/task (.*?):/m, 1]
-    "rake #{namespace}:#{task}"
+    rake_files = Dir[rake_migrations_lib].map do |file|
+      if !results.include?(file[/\d+/])
+        file = File.read(file)
+        namespace = file[/namespace :(.*?)do/m, 1].strip
+        task = file[/task (.*?):/m, 1]
+        "rake #{namespace}:#{task}"
+      end
+    end.compact
+
+    if !rake_files.empty?
+      puts "\n"
+      puts "You need to run the following rakes:"
+      puts "------------------------------------"
+      rake_files.each { |file| puts "\e[31m#{file}\e[0m" }
+      puts "\n"
+    end
   end
-end.compact
-
-if !rake_files.empty?
-  puts "\n"
-  puts "You need to run the following rakes:"
-  puts "------------------------------------"
-  rake_files.each { |file| puts "\e[31m#{file}\e[0m" }
-  puts "\n"
 end
+
+RakeMigrationsCheck.check
